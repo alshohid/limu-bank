6791d44c9d451887812921b693c23b831802a511
alshohid <alshohid1997@gmail.com>
2022-08-24 10:07:40 +0600

    adding html, js and css file

---
+++ limu-bank/Index.js
@@ -0,0 +1,6 @@
+
+const loginBtn = document.querySelector("#login-btn");
+loginBtn.addEventListener("click",function(){
+    const loginArea = document.querySelector("#login-area");
+    loginArea.style.display = "none";
+})
